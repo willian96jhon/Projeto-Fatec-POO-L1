915c39740070fc327df9ecc87446a94e14a41662
willian96jhon <70404640+willian96jhon@users.noreply.github.com>
2021-03-14 16:38:31 -0300

    Add files via upload

--- teste/src/main/java/com/fatec/teste/controller/CadastroController.java
+++ teste/src/main/java/com/fatec/teste/controller/CadastroController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.fatec.teste.db.PessoaRepository;
+import com.fatec.teste.model.Cliente;
 import com.fatec.teste.model.Pessoa;
+import com.fatec.teste.model.Telefone;
 
 @Controller
 public class CadastroController {
@@ -21,9 +23,11 @@ public class CadastroController {
 
     
     @PostMapping("/cadastrar")
-  	public String iniv(Pessoa p) {
-    	pr.save(p);
+  	public String iniv(Cliente c, Telefone t) {
+    	c.getTelefones().add(t);
+    	pr.save(c);
     	
+    	System.out.println(c.getGenero() + c.getNascimento() );
   		return "cadastro";
   	}
 }
